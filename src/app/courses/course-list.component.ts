ecb80998b713b89a14b0278405070866e616fab8
arilson.almada <arilson.almada@gmail.com>
2021-07-11 10:19:04 -0300

    Inserting images and rating stars

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -16,21 +16,21 @@ export class CourseListComponent implements OnInit{
             {
                 id: 1,
                 name: 'Angular',
-                imageUrl: '',
+                imageUrl: '/assets/images/angular.png',
                 price: 99.99,
                 code: 'XPS-8796',
                 duration: 120,
-                rating: 5.4,
+                rating: 4.5,
                 releaseDate: 'November, 2 , 2019'
             },
             {
                 id: 2,
                 name: 'React',
-                imageUrl: '',
+                imageUrl: '/assets/images/react.png',
                 price: 99.99,
                 code: 'LKL-8796',
                 duration: 80,
-                rating: 6.4,
+                rating: 3.4,
                 releaseDate: 'December, 4 , 2019'
             }
         ];
